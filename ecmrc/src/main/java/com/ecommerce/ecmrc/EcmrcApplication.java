cccc8a92fa23b126f76fd2d2d3a2038444967045
Mahdy Muzzammil <156871399+HeyMahdy@users.noreply.github.com>
2024-11-05 00:55:51 +0600

    Initial commit

---
+++ ecmrc/src/main/java/com/ecommerce/ecmrc/EcmrcApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecmrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmrcApplication.class, args);
+	}
+
+}
---
+++ ecmrc/src/test/java/com/ecommerce/ecmrc/EcmrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecmrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
